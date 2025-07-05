d799a749cc3045480eef358291a726d23c9d1ac3
Tralenor <tralenor@gmail.com>
2025-07-05 09:35:05 +0200

    correct loading hook naming

--- src/ReagentBank_loader.cpp
+++ src/ReagentBank_loader.cpp
@@ -8,7 +8,7 @@ void AddSC_mod_reagent_bank();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addmod_reagent_bankScripts()
+void Addmod_guild_reagent_bankScripts()
 {
     AddSC_mod_reagent_bank();
 }
